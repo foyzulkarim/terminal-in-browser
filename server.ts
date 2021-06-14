5590e9295178578eaf9121cf4a3e947a39f9e198
Foyzul Karim <foyzul@seekasia.com>
2021-06-14 22:14:25 +0800

    implemented kill process and added socket.io

--- server.ts
+++ server.ts
@@ -1,7 +1,17 @@
 import express = require("express");
+const find = require("find-process");
+import { Server, Socket } from "socket.io";
 import { run } from "./executor";
 // Create a new express app instance
 const app: express.Application = express();
+
+const httpServer = require("http").createServer(app);
+const options = {  };
+const io = require("socket.io")(httpServer, options);
+
+io.on("connection", (socket: Socket) => {
+  console.log('connected');
+});
 
 app.get("/", function (req: express.Request, res: express.Response) {
   console.log(req.params, req.query);
@@ -10,13 +20,48 @@ app.get("/", function (req: express.Request, res: express.Response) {
 
 app.get("/execute", function (req: express.Request, res: express.Response) {
   const command = req.query.command as string;
-  run(command).then(function (result: any) {
-    console.log(result);
-    res.send(result.data);
-  })
-  .catch(function (err) {
-    res.send(err);
-  });
+  run(command)
+    .then(function (result: any) {
+      // console.log(result);
+      res.send(result.data);
+    })
+    .catch(function (err) {
+      res.send(err);
+    });
+});
+
+app.get("/getpid", function (req: express.Request, res: express.Response) {
+  const pid = req.query.pid as string;
+  find("pid", parseInt(pid)).then(
+    function (list: any) {
+      console.log(list);
+      res.send(list);
+    },
+    function (err: any) {
+      console.log(err.stack || err);
+      res.send(err);
+    }
+  );
+});
+
+app.get("/getpid", function (req: express.Request, res: express.Response) {
+  const pid = req.query.pid as string;
+  find("pid", parseInt(pid)).then(
+    function (list: any) {
+      console.log(list);
+      res.send(list);
+    },
+    function (err: any) {
+      console.log(err.stack || err);
+      res.send(err);
+    }
+  );
+});
+
+app.get("/kill", function (req: express.Request, res: express.Response) {
+  const pid = req.query.pid as string;
+  process.kill(parseInt(pid));
+  res.send("Killed");
 });
 
 app.listen(3000, function () {
--- worker.ts
+++ worker.ts
@@ -1,6 +1,5 @@
 import { parentPort, workerData, MessagePort } from "worker_threads";
 import { exec, ChildProcess } from "child_process";
-import { Readable } from "stream";
 
 const cb = (e: any, d: string, se: string): void => {
   console.log('DONE',d);
@@ -10,10 +9,10 @@ const cb = (e: any, d: string, se: string): void => {
 };
 
 const proc: ChildProcess = exec(workerData.command, cb);
-// console.log(proc.stdout as Readable);
+console.dir(proc.pid);
 if (proc != null && proc.stdout != null) {
   proc.stdout.on("data", (data: string) => {
-    console.log('ONGOING', data);
+    // console.log('ONGOING', data);
     // const p = parentPort as MessagePort;
     //p.postMessage({flag: 'ONGOING', data: data});
   });
